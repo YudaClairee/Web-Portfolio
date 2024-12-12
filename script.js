ed59482590f4185e1aadd14fed2730ebc0a10168
YudaClairee <djibyuda@gmail.com>
2024-12-12 08:50:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('scroll', () => {
+  const navbar = document.querySelector('header');
+
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+})
